6249b337903c4e6b6964545bd275721ce13bc1c2
Developer <dev@talos-autoscaler.local>
2026-08-27 16:33:02 +0000

    chore: run two stateless replicas with autoscaler_tag config

--- autoscaler/main.go
+++ autoscaler/main.go
@@ -77,7 +77,6 @@ func main() {
 		BaseGPUVMID:  baseGPUVMID,
 		WorkerPrefix: workerPrefix,
 		GPUPrefix:    gpuPrefix,
-		InFlight:     make(map[string]bool),
 	}
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
